8d12337cada7ae34ce74176f9b2c722343ddc8dc
canadmin <icanyard@gmail.com>
2019-12-11 20:51:08 +0300

    fixed project dashboard

--- src/main/java/com/pms/projectmanagement/controllers/TaskController.java
+++ src/main/java/com/pms/projectmanagement/controllers/TaskController.java
@@ -3,6 +3,7 @@ package com.pms.projectmanagement.controllers;
 import com.pms.projectmanagement.dtos.ProjectDto;
 import com.pms.projectmanagement.dtos.TaskDto;
 import com.pms.projectmanagement.dtos.UserDto;
+import com.pms.projectmanagement.services.project.ProjectService;
 import com.pms.projectmanagement.services.task.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -18,7 +19,7 @@ import java.util.UUID;
 public class TaskController {
 
     private final TaskService taskService;
-
+    private final ProjectService projectService;
     @RequestMapping("/createTask/{projectId}")
     public String createNewTask(@PathVariable UUID projectId,
                                 @SessionAttribute("user")UserDto userDto,
@@ -27,7 +28,9 @@ public class TaskController {
                                 Model model,
                                 @RequestParam(value = "username",required = false) String username){
         taskService.createNewTask(projectId,userDto.getId(),taskDto,username);
+        ProjectDto newProjectDto = projectService.getProjectInfo(projectDto.getId());
         model.addAttribute("task",new TaskDto());
+        model.addAttribute("projectInfo",newProjectDto);
         return "project/project";
 
     }
